980a3fc4f0bd853b436b26db9c0dbef848607347
Larissa Narumi Takeda <narumitakeda97@gmail.com>
2018-01-05 10:03:49 -0200

    Update main.c

--- main.c
+++ main.c
@@ -2,26 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <time.h>
+
 #include "Rawr1.h"
 
 
 
 int main()
 {
-    int op;
 
-
-    printf("\t\t\t\t\t\t\tMENU\n\n");
-    printf("\t\t\t\t\t\t1 - NOVO JOGO\n");
-    setCursor(0, 0);
-    scanf("%d", &op);
-
-    switch(op)
-    {
-    case 1:
-        jogo();
-        break;
-    }
+    imprime_menu();
 
     setCursor(0, 24);
     return 0;
