6db24645e22520f0da9fe3ea515beca58c91ec8c
Lipe17 <35049103+Lipe17@users.noreply.github.com>
2018-01-07 17:55:53 -0200

    Update library.h

--- library.h
+++ library.h
@@ -1,3 +1,4 @@
+
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
@@ -6,29 +7,38 @@
 #define MAX_OBS 2
 #define MAX_DIST 99
 
+
 typedef struct{
-    int posX;
-    int posY;
-    int estadoDino;
-}DINO;
+    int Dino_x, Dino_y;
+    int Dino_pulo, Dino_down;
+    int Dino_andou, ApagaUmaVez;
+    int pos;
+    int Obs1_x, Obs1_y;
+    int Obs1_obstaculo, Obs1_estado;        //usada para salvar as variaveis do jogo em um arquivo binario e carregar do arquivo binario para as variaveis do jogo
+    int Obs2_x, Obs2_y;
+    int Obs2_obstaculo, Obs2_estado;
+    int vidas, pontuacao, nivel;
+    char nome[20];
+    int ciclo_pulo;
+}SALVAR;
 
 typedef struct{
     int vidas;
-    int pontuacao;
+    int pontuacao;                          //informacoes do jogo
     int nivel;
     char nome[20];
 }GAME_INFO;
 
 typedef struct{
     int x;
-    int y;
+    int y;                                  //usada para os obstaculos
     int obstaculo;
-    int estado;     // se esta no jogo ou nao
+    int estado;     // se esta na tela ou nao
 }OBSTACULO;
 
 typedef struct{
     int superior;
-    int inferior;
+    int inferior;                           //definicao dos limites dos obstaculos e do dinossauro
     int esquerda;
     int direita;
 }LIMITE;
@@ -36,10 +46,13 @@ typedef struct{
 typedef struct
 {
     int pontos;
-    char nome[20];
+    char nome[20];                          //usada no ranking
 }RANKING;
 
+
+//Manipulacao da tela
 void setCursor(int x, int y);
+void hideCursor(void);
 void imprimeChao(void);
 void imprimeDino(int x, int y);
 void apagaDino(int x, int y);
@@ -51,32 +64,38 @@ void imprimeCactoP(int x, int y);
 void apagaCactoP(int x, int y);
 void imprimeAerolito(int x, int y);
 void apagaAerolito(int x, int y);
-void imprimePterotactilo(int x, int y);
+void imprimePterotactilo1(int x, int y);
+void imprimePterotactilo2(int x, int y);
 void apagaPterotactilo(int x, int y);
 void imprimeCactoG(int x, int y);
 void apagaCactoG(int x, int y);
-int imprimeObstaculo(int x, int y, int obstaculo, LIMITE *obs);
+int imprimeObstaculo(int x, int y, int obstaculo, LIMITE *obs, int Andou);  //eh usada para definicao de limites tambem
 void apagaObstaculo(int x, int y, int obstaculo);
+void limpaMenu(void);
+void limpaTela(void);
+
+//Jogo e teste de colisao
+void novo_jogo(SALVAR buffer, int carregou);
 void limiteDino(int x, int y, LIMITE *Dino, int Dino_down);
 int testeColisao(LIMITE Dino, LIMITE Obs);
 void nivelEvelocidade(GAME_INFO *status, int *velocidade);
+void resetaLimite(LIMITE *Obs);
 
-
+//Menus
 int menuPause(GAME_INFO *status);
-void imprime_menu(void);
-void limpaMenu(void);
-void novo_jogo(void);
+int imprime_menu(void);
+
+
+
+//manipulacao de arquivos
 void carrega_jogo(void);
 void top_10(void);
-
 void adiciona_score(char* nome, int pontos);
+void atualiza_hiscore(char* nome, int pontos);
 void ordena_ranking(RANKING* ranking, int tamanho);
-void resetaLimite(LIMITE *Obs);
-void salvaJogo(int Dino_x, int Dino_y, int Dino_pulo, int Dino_down, int Dino_Andou, int ApagaUmaVez, OBSTACULO Obs1, OBSTACULO Obs2, int pos, GAME_INFO status);
-
+void salvaJogo(int Dino_x, int Dino_y, int Dino_pulo, int Dino_down, int Dino_Andou, int ApagaUmaVez, OBSTACULO Obs1, OBSTACULO Obs2, int pos, GAME_INFO status, int ciclo_pulo);
+void mensagemFimdeJogo(void);
 #endif // Rawr1.h
-
-
 
 
 
