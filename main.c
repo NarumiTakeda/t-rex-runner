8833d08ad933fe20abd8b907d0b477ca61cd59c4
Lipe17 <35049103+Lipe17@users.noreply.github.com>
2018-01-07 17:56:28 -0200

    Update main.c

--- main.c
+++ main.c
@@ -4,31 +4,19 @@
 #include <windows.h>
 #include <time.h>
 #include "conio2.h"
-
-
-
 #include "library.h"
-
-
 
 int main()
 {
-    int i, j;
 
-    //vou passar fazer uma funcao pra isso depois
-    for(i = 0; i < 20; i++){
-        for(j = 0; j < 100; j++){
-            textbackground(WHITE);
-            printf(" ");
-        }
-    }
+    hideCursor();
 
-    textbackground(WHITE);
-    textcolor(BLACK);
+    limpaTela();
 
     imprime_menu();  // Chamada da função que imprime o menu
 
-    setCursor(0, 24);  // Define que "return 0" ficará no canto esquerdo 
-    return 0;          // embaixo na tela
+
+    return 0;
 }
+
 
