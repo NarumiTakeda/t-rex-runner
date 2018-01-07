961ba2648b0717b810ddd5c37c2070ebea45cb33
Lipe17 <35049103+Lipe17@users.noreply.github.com>
2018-01-06 22:49:20 -0200

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <windows.h>
 #include <time.h>
+#include "conio2.h"
+
+
 
 #include "Rawr1.h"
 
@@ -10,6 +13,18 @@
 
 int main()
 {
+    int i, j;
+
+    //vou passar fazer uma funcao pra isso depois
+    for(i = 0; i < 20; i++){
+        for(j = 0; j < 100; j++){
+            textbackground(WHITE);
+            printf(" ");
+        }
+    }
+
+    textbackground(WHITE);
+    textcolor(BLACK);
 
     imprime_menu();
 
